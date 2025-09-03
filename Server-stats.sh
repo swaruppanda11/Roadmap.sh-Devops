7d6ae8ab6b922ad77a09a32d1278fae562f5b311
swaruppanda11 <swaruppanda11@gmail.com>
2025-09-03 16:53:45 -0600

    minor changes

--- Server-stats.sh
+++ Server-stats.sh
@@ -3,6 +3,10 @@
 echo "=================== Service Performance Stats ===================" 
 echo "Hostname          : $(hostname)"
 
+print metric() {
+    echo -e "${YELLOW}$1:${NC} $2"
+}
+
 if [ -f /etc/os-release ]; then
     # Linux
     OS=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d \")
@@ -17,3 +21,7 @@ else
 fi
 
 echo "OS Version      : $OS"
+
+get_cpu_usage()
+echo “CPU Usage:”
+ps -A -o %cpu | awk '{s+=$1} END {print s "%"}'
