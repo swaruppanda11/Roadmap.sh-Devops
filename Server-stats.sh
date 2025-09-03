1865ba302090244816b1fa5ee54a930895c4fa3f
swaruppanda11 <swaruppanda11@gmail.com>
2025-09-03 10:41:06 -0600

    identifying different OS Versions

--- Server-stats.sh
+++ Server-stats.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 echo "=================== Service Performance Stats ===================" 
-echo "
+echo "Hostname          : $(hostname)"
+
+if [ -f /etc/os-release ]; then
+    # Linux
+    OS=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d \")
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    OS="$(sw_vers -productName) $(sw_vers -productVersion)"
+elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32" ]]; then
+    # Windows (Git Bash, Cygwin, MSYS)
+    OS="Windows ($(cmd.exe /c ver | tr -d '\r'))"
+else
+    OS="Unknown OS"
+fi
+
+echo "OS Version      : $OS"
